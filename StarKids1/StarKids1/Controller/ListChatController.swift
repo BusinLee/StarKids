074ef52ab073652e4d98c9515066b65f9238beaa
Thanh Lê <thanhle@Thanhs-Mac.local>
2019-04-16 08:38:44 -0700

    get data from fb

--- StarKids1/StarKids1/Controller/ListChatController.swift
+++ StarKids1/StarKids1/Controller/ListChatController.swift
@@ -13,6 +13,7 @@ var currentUser:User!
 
 class ListChatController: UIViewController {
 
+    var listFriend:Array<User> = Array<User>()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,6 +31,20 @@ class ListChatController: UIViewController {
             let userId = tableName.child(currentUser.id)
             let user:Dictionary<String,String> = ["email":currentUser.email,"fullName":currentUser.fullName,"linkAvatar":currentUser.linkAvatar]
             userId.setValue(user)
+            
+            tableName.observe(.childAdded, with: { (snapshot) in
+                let postDict = snapshot.value as? [String: AnyObject]
+                if (postDict != nil) {
+                    print("++++++++ \(postDict)")
+                    let email:String = (postDict?["email"])! as! String
+                    let fullName:String = (postDict?["fullName"])! as! String
+                    let linkAvatar:String = (postDict?["linkAvatar"])! as! String
+                    
+                    let user:User = User(id: snapshot.key, email: email, fullName: fullName, linkAvatar: linkAvatar)
+                    self.listFriend.append(user)
+                    print("-------- \(self.listFriend)")
+                }
+            })
         } else {
             print("Không có user")
         }
